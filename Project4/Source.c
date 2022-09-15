32ca5a7cf290670cbb4f0ed6736c8b6d88f6b1ba
16020 <16020@DESKTOP-TU644NC>
2022-09-15 17:32:49 +0300

    Добавьте файлы проекта.

---
+++ Project4/Source.c
@@ -0,0 +1,17 @@
+#include <locale.h>
+
+#include <stdio.h>
+
+void main()
+{
+    puts("                  /\\                ");
+    puts("                 /  \\               ");
+    puts("                 |  |                ");
+    puts("                 |  |                ");
+    puts("                 |  |                ");
+    puts("                 |  |                ");
+    puts("                /|  |\\              ");
+    puts("               / |  | \\             ");
+    puts("              /  |  |  \\            ");
+
+}
